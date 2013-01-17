5ccdabf2098d8d424b9ca9b3f7e88952dd711bdd
dahn <dansuri2@gmail.com>
2013-01-17 14:07:32 +0900

    dfaefaw

---
+++ CPPSourceCodes/TodayHumor_Questions/121207_scanf/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(void)
+{
+	while(1)
+	{
+		char rktp = '\0', threash;
+		printf("12345->");
+
+		scanf("%c", &rktp);
+		scanf("%c", &threash);
+		//printf("\n");
+		
+
+		int a = 0;
+	}
+
+	return 0;
+}
